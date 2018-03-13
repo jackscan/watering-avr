eafb474c42bd96de6b2bbb8084d83a5d83e20ff5
Mathias Fiedler <mathias.fiedler@gmail.com>
2018-03-13 18:42:46 +0100

    use timer2 for clock monotonic

--- timer.c
+++ timer.c
@@ -7,20 +7,19 @@
 uint32_t s_count = 0;
 
 void timer_start(void) {
-    PRR &= ~(1 << PRTIM0);
+    PRR &= ~(1 << PRTIM2);
     // timer overflow interrupt enabled
-    TIMSK0 = (1 << TOIE0);
+    TIMSK2 = (1 << TOIE2);
     // 1/1024
-    TCCR0B = (1 << CS02) | (1 << CS00);
-    // TCCR1B = (1 << CS12);
-    TCNT0 = 0;
+    TCCR2B = (1 << CS22) | (1 << CS21) | (1 << CS20);
+    TCNT2 = 0;
 }
 
-ISR (TIMER0_OVF_vect) {
+ISR (TIMER2_OVF_vect) {
     ++s_count;
 }
 
 uint32_t get_time(void) {
-    uint8_t c = TCNT0;
+    uint8_t c = TCNT2;
     return (s_count << 8) | (uint32_t)c;
 }
